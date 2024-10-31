a09cc6e658994f9f7c8eada57d28e7e82b8bbc1c
niharhEAVR <debnathnihar14@gmail.com>
2024-10-31 23:34:08 +0530

    answers

--- async.js
+++ async.js
@@ -1,1 +1,12 @@
-console.log('nono')
+function setTimeoutPromisified(miliSec) {
+    return new Promise(resolve => setTimeout(resolve,miliSec))
+}
+
+(async()=>{
+   await setTimeoutPromisified(1000)
+   console.log('Hi') 
+   await setTimeoutPromisified(3000)
+   console.log('Hello') 
+   await setTimeoutPromisified(5000)
+   console.log('Hello There...') 
+})()
